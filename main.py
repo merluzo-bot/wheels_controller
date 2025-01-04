435f1c8364406c8d803df2e9542f414b7c7eeba2
Alonso <81057787+rir001@users.noreply.github.com>
2025-01-04 18:34:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,94 +1,85 @@
-import uasyncio as asyncio
-from machine import Pin
+from machine import Pin, PWM, UART
+from time import sleep
+
+uart = UART(1, baudrate=115200, tx=Pin(4), rx=Pin(5))
+
+class Motor:
+    max_vel = 800
+    def __init__(self, step:int, direction:int, enable:int):
+        self.step_pin = Pin(step, Pin.OUT)
+        self.step = PWM(self.step_pin)
+        self.step.duty_u16(512)
+        self.step.freq(100)
+
+        self.dir = Pin(direction, Pin.OUT)
+
+        self.enable = Pin(enable, Pin.OUT)
+        self.enable.value(0)
+
+    def set_motor(self, vel, direction):
+        print(100 + int(vel*self.max_vel))
+        print(direction)
+        self.step.freq(100 + int(vel*self.max_vel))
+        self.dir.value(not direction)
 
 
-step = Pin(21, Pin.OUT)
-dir = Pin(20, Pin.OUT)
-step2 = Pin(10, Pin.OUT)
-dir2 = Pin(11, Pin.OUT)
+L_motor = Motor(21, 20, 22)
+L_motor.dir.value(1)
+R_motor = Motor(10, 11, 12)
+R_motor.dir.value(0)
+
 led = Pin(25, Pin.OUT)
+btn = Pin(24, Pin.IN, Pin.PULL_UP)
+
+val = False
+
+def exec_message(message):
+    print(message)
+
+    if message[0] == "S":
+        L_motor.enable.value(0)
+        R_motor.enable.value(0)
+        L_motor.set_motor(0, 0)
+        R_motor.set_motor(0, 0)
+
+    if message[0] == "R":
+        direction = 0 if message[2] == "+" else 1
+        vel = int(message[3:6])
+        R_motor.enable.value(1)
+        R_motor.set_motor(vel/255, direction)
+
+    if message[0] == "L":
+        direction = 1 if message[2] == "+" else 0
+        vel = int(message[3:6])
+        L_motor.enable.value(1)
+        L_motor.set_motor(vel/255, direction)
+
+    if message[0] == "A":
+        direction = 1 if message[2] == "+" else 0
+        vel = int(message[3:6])
+        R_motor.enable.value(1)
+        R_motor.set_motor(vel/255, not direction)
+        L_motor.enable.value(1)
+        L_motor.set_motor(vel/255, direction)
+
+    if message[0] == "D":
+        R = int(message[2:5])
+        L = int(message[6:9])
+        R_motor.enable.value(1)
+        R_motor.set_motor(R/255, 0 if message[1] == "+" else 1)
+        L_motor.enable.value(1)
+        L_motor.set_motor(L/255, 1 if message[5] == "+" else 0)
 
 
-async def adelante():
-    dir.value(0)
-    dir2.value(1)
-    while True:
-        step.value(1)
-        step2.value(1)
-        led.value(1)
-        await asyncio.sleep(0.001)
-        step.value(0)
-        step2.value(0)
-        led.value(0)
-        await asyncio.sleep(0.001)
-
-async def atras():
-    dir.value(1)
-    dir2.value(0)
-    while True:
-        step.value(1)
-        step2.value(1)
-        led.value(1)
-        await asyncio.sleep(0.001)
-        step.value(0)
-        step2.value(0)
-        led.value(0)
-        await asyncio.sleep(0.001)
-
-async def derecha():
-    dir.value(0)
-    dir2.value(0)
-    while True:
-        step.value(1)
-        step2.value(1)
-        led.value(1)
-        await asyncio.sleep(0.001)
-        step.value(0)
-        step2.value(0)
-        led.value(0)
-        await asyncio.sleep(0.001)
-
-async def izquierda():
-    dir.value(1)
-    dir2.value(1)
-    while True:
-        step.value(1)
-        step2.value(1)
-        led.value(1)
-        await asyncio.sleep(0.001)
-        step.value(0)
-        step2.value(0)
-        led.value(0)
-        await asyncio.sleep(0.001)
-
-async def stop():
-    step.value(0)
-    step2.value(0)
-    led.value(0)
-
-
-async def command_handler():
-    while True:
-        command = input("Instruccion: ").strip().lower()
-
-        if command == "adelante":
-            asyncio.create_task(adelante())
-        elif command == "atras":
-            asyncio.create_task(atras())
-        elif command == "derecha":
-            asyncio.create_task(derecha())
-        elif command == "izquierda":
-            asyncio.create_task(izquierda())
-        elif command == "stop":
-            await stop()
+message = ""
+while True:
+    if uart.any():
+        char = uart.read().decode()
+        if char == "_":
+            pass
+        elif char == "*":
+            if message != "": exec_message(message)
+            message = ""
         else:
-            print("Invalid command")
+            message += char
 
-        await asyncio.sleep(0)
-
-
-async def main():
-    await command_handler()
-
-
-asyncio.run(main())
